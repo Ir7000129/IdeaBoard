9cbfc5ca1924e63290c1055aea6a82544e0140ac
Ir7000129 <64563425+Ir7000129@users.noreply.github.com>
2020-09-03 20:41:09 +1200

    03/09

---
+++ IdeaBoard/src/com/IdeaBoard/Model/TextBoxStream.java
@@ -0,0 +1,14 @@
+
+package com.IdeaBoard.Model;
+
+import java.util.List;
+
+public class TextBoxStream {
+	List<String> next = List.of("A", "b");
+	
+	for (var item:next) {
+		System.out.print(item);
+	}
+}
+
+}
